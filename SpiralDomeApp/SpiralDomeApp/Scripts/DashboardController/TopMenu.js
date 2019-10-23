9522aec82aef65c5ca2d47c6ef3bfb7d01233047
Shuhab <chistiya@gmail.com>
2019-10-23 07:58:08 -0400

    Add new icons for top menu

--- SpiralDomeApp/SpiralDomeApp/Scripts/DashboardController/TopMenu.js
+++ SpiralDomeApp/SpiralDomeApp/Scripts/DashboardController/TopMenu.js
@@ -12,9 +12,10 @@ class TopMenu extends React.Component {
                 <nav aria-label="Page navigation example">
                     <ul className="pagination justify-content-center" style={ulTop}>
                         <li className="page-item"><a className="page-link" href='#'> <img style={imgIcon} src='../Content/images/logo.png'></img></a></li>
-                        <li className="page-item"><a className="page-link" href='#'> <input type='text' className='form-control' placeholder='Search by Name' /></a></li>
-                        <li onClick={this.accountAction} className="page-item"><a id="menuAcct" className="page-link" href="#"> <img style={imgIcon} src='../Content/images/account.png'></img> Account</a></li>
-                        <li onClick={this.logoutAction}><a className="page-link" href="#"><img style={imgIcon} src='../Content/images/logout.png'></img> Logout</a></li>
+                        <li className="page-item"><a className="page-link" href='#'> <input type='text' className='form-control' placeholder='Search by Name' /></a> &nbsp;</li>
+                        <li className="page-item"><a className="page-link" href='#'> <button className='btn btn-primary'>Search</button></a></li>
+                        <li onClick={this.accountAction} className="page-item"><a id="menuAcct" className="page-link" href="#"> <img style={imgIcon} src='../Content/images/account.svg'></img> Account</a></li>
+                        <li onClick={this.logoutAction}><a className="page-link" href="#"><img style={imgIcon} src='../Content/images/logout.svg'></img> Logout</a></li>
                     </ul>
                 </nav>
             </React.Fragment>
